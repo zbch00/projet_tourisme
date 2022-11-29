cd7e9e79c5fdbf586c15266bf9199fa90032cc66
Chaiboubi Zeineb <zbch33@gmail.com>
2022-11-29 16:40:31 +0100

    ajout nav

---
+++ src/Controller/EtablissementController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EtablissementController extends AbstractController
+{
+    #[Route('/etablissement', name: 'app_etablissement')]
+    public function index(): Response
+    {
+        return $this->render('etablissement/index.html.twig', [
+            'controller_name' => 'EtablissementController',
+        ]);
+    }
+}
